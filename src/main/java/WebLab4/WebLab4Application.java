dcb0787306dc79871ed3260a42186fa4902d1637
DIVAYTH <dp.acid12@gmail.com>
2021-01-25 02:54:52 +0300

    WebLab

---
+++ src/main/java/WebLab4/WebLab4Application.java
@@ -0,0 +1,12 @@
+package WebLab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLab4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebLab4Application.class, args);
+	}
+}
